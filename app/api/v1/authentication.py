aea71627096942f6da1583b798c280d1f1060785
Dushani96 <168927373+Dushani96@users.noreply.github.com>
2025-12-11 17:50:29 +0530

    token refresh

--- app/api/v1/authentication.py
+++ app/api/v1/authentication.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter, HTTPException, Depends
 from app.api.v1.schemas.authentication import SignIn
-from app.utils.validation import generate_signature
+from app.utils.validation import generate_signature,refresh_signature
 
 router = APIRouter()
 
@@ -17,11 +17,27 @@ async def create_news(user: SignIn):
 
         if data["username"] == userName and data["password"] == password:
             print("Login Successful")
+
             token = await generate_signature({"username": data["username"]})
+            refresh_token = await refresh_signature({"username": data["username"]})
             print(token)
-            return {"status": True, "message": "Login Successful", "token": token}
+            
+            return {"status": True, "message": "Login Successful", "token": token, "refresh_token": refresh_token}
 
         return {"status": False, "message": "Invalid username or password"}
+
+    except Exception as e:
+        return {"status": False, "message": str(e)}
+    
+@router.post("/validate-token")
+async def refresh_token_endpoint(token: str):
+    try:
+        if not token:
+            return {"status": False, "message": "Token is missing"}
+
+        new_token = await generate_signature({"username": "admin"})
+        refresh_token = await refresh_signature({"username": "admin"})
+        return {"status": True, "message": "Token refreshed successfully", "token": new_token, "refresh_token": refresh_token}
 
     except Exception as e:
         return {"status": False, "message": str(e)}
